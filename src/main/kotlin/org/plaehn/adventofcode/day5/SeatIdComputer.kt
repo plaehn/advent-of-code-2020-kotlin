8721116cc07bf9fe5d24484213aa8cec060bafbe
plaehn.oliver <plaehn.oliver@swm.de>
2020-12-05 13:38:37 +0100

    Refactor out duplicated code and use fold

--- src/main/kotlin/org/plaehn/adventofcode/day5/SeatIdComputer.kt
+++ src/main/kotlin/org/plaehn/adventofcode/day5/SeatIdComputer.kt
@@ -6,27 +6,18 @@ import org.plaehn.adventofcode.common.upperHalf
 class SeatIdComputer {
 
     fun computeSeatId(binarySpacePartition: String): Int {
-        var rowRange = 0..127
-        for (rowChar in binarySpacePartition.dropLast(3)) {
-            rowRange = if (rowChar == 'F') {
-                rowRange.lowerHalf()
-            } else {
-                rowRange.upperHalf()
-            }
-        }
-        val row = rowRange.first
-
-        var colRange = 0..7
-        for (colChar in binarySpacePartition.drop(7)) {
-            colRange = if (colChar == 'L') {
-                colRange.lowerHalf()
-            } else {
-                colRange.upperHalf()
-            }
-        }
-        val col = colRange.first
-
+        val row = foldRange(0..127, binarySpacePartition.dropLast(3), 'F')
+        val col = foldRange(0..7, binarySpacePartition.drop(7), 'L')
         return row * 8 + col
     }
+
+    private fun foldRange(inputRange: IntRange, binarySpacePartition: String, lowerHalfChar: Char): Int =
+            binarySpacePartition.fold(inputRange) { range, char ->
+                if (char == lowerHalfChar) {
+                    range.lowerHalf()
+                } else {
+                    range.upperHalf()
+                }
+            }.first
 }
 
