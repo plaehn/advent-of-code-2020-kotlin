08f470d41a5f8046279ec8b378c4f7eebe43784f
plaehn.oliver <plaehn.oliver@swm.de>
2020-12-11 10:52:37 +0100

    Refactorings

--- src/main/kotlin/org/plaehn/adventofcode/day10/JoltageDifferenceComputer.kt
+++ src/main/kotlin/org/plaehn/adventofcode/day10/JoltageDifferenceComputer.kt
@@ -8,25 +8,22 @@ class JoltageDifferenceComputer(adapterJoltages: List<Int>) {
     private val sortedJoltages = (adapterJoltages + listOf(3 + (adapterJoltages.maxOrNull() ?: 0))).sorted()
 
     fun computeJoltageDifferenceDistributionProduct(): Int =
-            sortedJoltages
-                    .mapIndexed { index, joltage -> joltage - previousJoltage(index) }
+            computeJoltageDifferences()
                     .groupingBy { it }
                     .eachCount()
                     .map { it.value }
                     .product()
 
+    private fun computeJoltageDifferences(): List<Int> = sortedJoltages
+            .mapIndexed { index, joltage -> joltage - previousJoltage(index) }
+
     private fun previousJoltage(index: Int): Int = if (index == 0) 0 else sortedJoltages[index - 1]
 
     fun countDistinctValidArrangements(): Long {
-        val joltageDifferences = sortedJoltages
-                .mapIndexed { index, joltage -> joltage - previousJoltage(index) }
-
-        val chunks = chunkByEqualElements(joltageDifferences.asSequence()).toList()
+        val chunks = chunkByEqualElements(computeJoltageDifferences().asSequence()).toList()
 
         val factors = chunks
-                .map {
-                    if (it.first() == 1) fibonacci(it.count() + 2) - 1 else 1
-                }
+                .map { if (it.first() == 1) fibonacci(it.count() + 2) - 1 else 1 }
                 .map { it.toLong() }
 
         return factors.product()
