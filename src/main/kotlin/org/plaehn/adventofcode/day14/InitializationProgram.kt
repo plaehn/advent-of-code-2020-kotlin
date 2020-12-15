e2e1f73c565ea57bb951fc0a77d7b412f8468713
Oliver Plaehn <oliver.plaehn@gmail.com>
2020-12-15 16:20:49 +0100

    Day 14, part 2: solution

--- src/main/kotlin/org/plaehn/adventofcode/day14/InitializationProgram.kt
+++ src/main/kotlin/org/plaehn/adventofcode/day14/InitializationProgram.kt
@@ -1,19 +1,29 @@
 package org.plaehn.adventofcode.day14
 
-import org.plaehn.adventofcode.day14.Bitmask.Bit.*
 import java.math.BigInteger
 
 
-class InitializationProgram(private val instructions: List<Instruction>) {
+class InitializationProgram(
+    private val instructions: List<Instruction>,
+    private val useVersionTwo: Boolean = false
+) {
 
-    private var currentBitmask: Bitmask = Bitmask.fromString("X".repeat(36))
-    private var memory: MutableMap<Int, Long> = mutableMapOf()
+    private var currentBitmask: Bitmask = Bitmask("X".repeat(36))
+    private var memory: MutableMap<Long, Long> = mutableMapOf()
 
     fun applyInstructions() {
-        instructions.forEach {
-            when (it) {
-                is Instruction.SetBitmask -> currentBitmask = it.bitmask
-                is Instruction.SetMemory -> memory[it.address] = currentBitmask.applyTo(it.value)
+        instructions.forEach { instruction ->
+            when (instruction) {
+                is Instruction.SetBitmask -> currentBitmask = instruction.bitmask
+                is Instruction.SetMemory -> {
+                    if (useVersionTwo) {
+                        // XXX use strategy?
+                        val allAddresses = currentBitmask.applyToVersionTwo(instruction.address)
+                        allAddresses.forEach { memory[it] = instruction.value }
+                    } else {
+                        memory[instruction.address] = currentBitmask.applyTo(instruction.value)
+                    }
+                }
             }
         }
     }
@@ -21,14 +31,14 @@ class InitializationProgram(private val instructions: List<Instruction>) {
     fun sumMemoryValues(): Long = memory.values.sum()
 
     companion object {
-        fun fromLines(lines: List<String>): InitializationProgram =
-            InitializationProgram(lines.map { Instruction.fromInputLine(it) })
+        fun fromLines(lines: List<String>, useVersionTwo: Boolean = false): InitializationProgram =
+            InitializationProgram(lines.map { Instruction.fromInputLine(it) }, useVersionTwo)
     }
 }
 
 sealed class Instruction {
     data class SetBitmask(val bitmask: Bitmask) : Instruction()
-    data class SetMemory(val address: Int, val value: Long) : Instruction()
+    data class SetMemory(val address: Long, val value: Long) : Instruction()
 
     companion object {
         fun fromInputLine(line: String): Instruction {
@@ -36,47 +46,48 @@ sealed class Instruction {
             val instruction = tokens[0]
             val value = tokens[1]
             return when (instruction) {
-                "mask" -> SetBitmask(Bitmask.fromString(value))
+                "mask" -> SetBitmask(Bitmask(value))
                 else -> {
                     val address = instruction.split('[', ']')[1]
-                    SetMemory(address.toInt(), value.toLong())
+                    SetMemory(address.toLong(), value.toLong())
                 }
             }
         }
     }
 }
 
-data class Bitmask(val bits: List<Bit>) {
-
-    enum class Bit {
-        ZERO, ONE, DO_NOT_CHANGE
-    }
+data class Bitmask(val bits: String) {
 
     fun applyTo(value: Long): Long =
         bits.foldIndexed(value) { index, modifiedValue, bit ->
             when (bit) {
-                ZERO -> setBit(index, modifiedValue, 0)
-                ONE -> setBit(index, modifiedValue, 1)
-                DO_NOT_CHANGE -> modifiedValue
+                '0' -> setBit(index, modifiedValue, 0)
+                '1' -> setBit(index, modifiedValue, 1)
+                'X' -> modifiedValue
+                else -> throw IllegalArgumentException()
             }
+        }
+
+    fun applyToVersionTwo(value: Long): List<Long> =
+        bits.foldIndexed(mutableListOf(value)) { index, modifiedValues, bit ->
+            val newList = mutableListOf<Long>()
+            modifiedValues.forEach { modifiedValue ->
+                when (bit) {
+                    '0' -> newList.add(modifiedValue)
+                    '1' -> newList.add(setBit(index, modifiedValue, 1))
+                    'X' -> {
+                        newList.add(setBit(index, modifiedValue, 0))
+                        newList.add(setBit(index, modifiedValue, 1))
+                    }
+                    else -> throw IllegalArgumentException()
+                }
+            }
+            newList
         }
 
     private fun setBit(index: Int, value: Long, bit: Int): Long {
         val binaryString = value.toString(2).padStart(36, '0')
         val changed = StringBuilder(binaryString).also { it.setCharAt(index, '0' + bit) }
         return BigInteger(changed.toString(), 2).toLong()
-    }
-
-    companion object {
-        fun fromString(input: String): Bitmask {
-            return Bitmask(input.map {
-                when (it) {
-                    'X' -> DO_NOT_CHANGE
-                    '1' -> ONE
-                    '0' -> ZERO
-                    else -> throw IllegalArgumentException("Cannot use char $it in bitmask")
-                }
-            })
-        }
     }
 }
--- src/test/kotlin/org/plaehn/adventofcode/day14/InitializationProgramTest.kt
+++ src/test/kotlin/org/plaehn/adventofcode/day14/InitializationProgramTest.kt
@@ -25,6 +25,26 @@ class InitializationProgramTest {
         assertThat(summedMemoryValues).isEqualTo(7997531787333)
     }
 
+    @Test
+    fun `Apply instructions using v2 and sum memory values for small input`() {
+        val initializationProgram = InitializationProgram.fromLines(readInput("small_input_v2.txt"), true)
+
+        initializationProgram.applyInstructions()
+        val summedMemoryValues = initializationProgram.sumMemoryValues()
+
+        assertThat(summedMemoryValues).isEqualTo(208)
+    }
+
+    @Test
+    fun `Apply instructions using v2 and sum memory values for large input`() {
+        val initializationProgram = InitializationProgram.fromLines(readInput("input.txt"), true)
+
+        initializationProgram.applyInstructions()
+        val summedMemoryValues = initializationProgram.sumMemoryValues()
+
+        assertThat(summedMemoryValues).isEqualTo(3564822193820)
+    }
+
     private fun readInput(resourceName: String): List<String> =
         InitializationProgram::class.java
             .getResource(resourceName)
