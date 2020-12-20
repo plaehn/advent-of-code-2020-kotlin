691e5f6728c9c6db1e722ae6abacca47e0d8a039
Oliver Plaehn <oliver.plaehn@gmail.com>
2020-12-20 16:36:01 +0100

    Day 20, part 1: solution

--- src/main/kotlin/org/plaehn/adventofcode/day20/JurassicJigsawSolver.kt
+++ src/main/kotlin/org/plaehn/adventofcode/day20/JurassicJigsawSolver.kt
@@ -1,8 +1,19 @@
 package org.plaehn.adventofcode.day20
 
 import org.plaehn.adventofcode.common.groupByBlankLines
+import org.plaehn.adventofcode.common.product
 
 class JurassicJigsawSolver(private val tiles: List<Tile>) {
+
+    fun computeProductOfCornerTileIds(): Long {
+        val allBorders = tiles.map { it.computeAllBorders() }.flatten()
+        val bordersWithCount = allBorders.groupingBy { it }.eachCount()
+        val uniqueBorders = bordersWithCount.filter { it.value == 1 }
+        val corners = tiles.filter { tile ->
+            4 == tile.computeAllBorders().count { uniqueBorders.contains(it) }
+        }
+        return corners.map { it.id }.product()
+    }
 
     override fun toString() = tiles.joinToString("\n\n")
 
@@ -12,15 +23,27 @@ class JurassicJigsawSolver(private val tiles: List<Tile>) {
     }
 }
 
-data class Tile(val id: Int, val rows: List<String>) {
+data class Tile(val id: Long, val rows: List<String>) {
+
+    fun computeAllBorders(): List<String> {
+        val borders = mutableListOf(
+            rows.first(),
+            rows.last(),
+            rows.map { it.first() }.joinToString(""),
+            rows.map { it.last() }.joinToString("")
+        )
+        borders.addAll(borders.map { it.reversed() })
+        return borders
+    }
 
     override fun toString() = "Tile $id:\n${rows.joinToString("\n")}"
 
     companion object {
         fun fromString(input: String): Tile {
             val lines = input.lines()
-            val id = lines.first().split(' ', ':')[1].toInt()
-            return Tile(id, lines.drop(1))
+            val id = lines.first().split(' ', ':')[1].toLong()
+            val rows = lines.drop(1).filter { it.isNotBlank() }
+            return Tile(id, rows)
         }
     }
 }
--- src/test/kotlin/org/plaehn/adventofcode/day20/JurassicJigsawSolverTest.kt
+++ src/test/kotlin/org/plaehn/adventofcode/day20/JurassicJigsawSolverTest.kt
@@ -1,14 +1,26 @@
 package org.plaehn.adventofcode.day20
 
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
 class JurassicJigsawSolverTest {
 
     @Test
-    fun `Parse input`() {
+    fun `Compute product of corner tiles for small input`() {
         val solver = JurassicJigsawSolver.fromString(readInput("small_input.txt"))
 
-        println(solver)
+        val product = solver.computeProductOfCornerTileIds()
+
+        assertThat(product).isEqualTo(20899048083289)
+    }
+
+    @Test
+    fun `Compute product of corner tiles for large input`() {
+        val solver = JurassicJigsawSolver.fromString(readInput("input.txt"))
+
+        val product = solver.computeProductOfCornerTileIds()
+
+        assertThat(product).isEqualTo(22878471088273)
     }
 
     private fun readInput(resourceName: String): String =
